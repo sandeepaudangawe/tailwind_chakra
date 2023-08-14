767abd12fa65b0ca3d5646d666383c502143160e
sandeepaudangawe <sandeepaudangawe@gmail.com>
2023-08-14 14:46:53 +0530

    adding files of Tailwind_Chakra

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="main-container">
+    <!-- <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a> -->
+
+    <div class="grid grid-cols-2 gap-4">
+       <div>first</div>
+       <div>
+            <div class="grid grid-cols-1 divide-y">
+               <div>01</div>
+               <div>02</div>
+               <div>03</div>
+               <div>04</div>
+               <div>05</div>
+               <div>06</div>
+            </div>
+       </div>
+    </div>
+  </div> 
+  <!-- <HelloWorld msg="Vite + Vue" /> --> 
+
+  <!-- <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1> -->
+
+</template>
+
+<style scoped>
+/* .logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+} */
+</style>
